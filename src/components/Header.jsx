67a26908345ea0f8351432836eef8a8981b8a7c9
Viktor Kikodze <vitland@gmail.com>
2022-04-11 23:35:08 +0300

    bugfix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import { Link, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-
-function Header({text, bgColor, textColor}) {
-
+function Header({ text, bgColor, textColor }) {
   const headerStyles = {
-            backgroundColor: bgColor,
-            color: textColor
-        }
+    backgroundColor: bgColor,
+    color: textColor,
+  };
 
   return (
     <header style={headerStyles}>
       <div className="container">
-      <h2>{text}</h2>
+        <h2>{text}</h2>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
-
+export default Header;
 
 Header.defaultProps = {
   text: 'Feedback UI',
   bgColor: 'rgba(0,0,0,0.4)',
-  textColor: '#3fe43f'
+  textColor: '#3fe43f',
 };
 
 Header.propTypes = {
-    text: PropTypes.string,
-    bgColor: PropTypes.string,
-    textColor: PropTypes.string
-}
-// export default Header;
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+
